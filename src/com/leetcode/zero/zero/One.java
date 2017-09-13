aad86baf6b24eb72aafe38469b4cec4b17b8300a
bugtrash <os@ma2oo.com>
2017-09-13 21:18:39 +0800

    first quiz

--- src/com/leetcode/zero/zero/One.java
+++ src/com/leetcode/zero/zero/One.java
@@ -1,7 +1,18 @@
 package com.leetcode.zero.zero;
 
+import java.util.HashMap;
+
 /**
  * Created by botao on 2017/9/13.
  */
 public class One {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            Integer needed = map.get(target - nums[i]);
+            if(needed != null) return new int[]{needed,i};
+            map.put(nums[i],i);
+        }
+        return new int[0];
+    }
 }
